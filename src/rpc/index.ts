71fa4fd8f61870c2471ef2e525b34fd9d8a7865b
Derrick Hammer <derrick@derrickhammer.com>
2022-04-30 00:41:35 -0400

    *Export and add sol methods

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -5,5 +5,6 @@ export * from "./common.js"
 import {default as DnsMethods} from "./dns.js"
 import {default as EvmMethods} from "./evm.js"
 import {default as HnsMethods} from "./handshake.js"
+import {default as SolMethods} from "./solana.js"
 
-export const rpcMethods: RpcMethodList = Object.assign({}, DnsMethods, EvmMethods, HnsMethods)
+export const rpcMethods: RpcMethodList = Object.assign({}, DnsMethods, EvmMethods, HnsMethods, SolMethods)
