0cda6144eb16ec0fcf2fc0e51d54455d2ad79881
Derrick Hammer <derrick@derrickhammer.com>
2022-05-21 15:56:33 -0400

    *Add algorand support

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -6,5 +6,13 @@ import {default as DnsMethods} from "./dns.js"
 import {default as EvmMethods} from "./evm.js"
 import {default as HnsMethods} from "./handshake.js"
 import {default as SolMethods} from "./solana.js"
+import {default as AlgoMethods} from "./algorand.js"
 
-export const rpcMethods: RpcMethodList = Object.assign({}, DnsMethods, EvmMethods, HnsMethods, SolMethods)
+export const rpcMethods: RpcMethodList = Object.assign(
+    {},
+    DnsMethods,
+    EvmMethods,
+    HnsMethods,
+    SolMethods,
+    AlgoMethods
+)
